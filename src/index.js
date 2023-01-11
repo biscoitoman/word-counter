f4ba351880c257560d2f1200796fc46c4349e1a7
MatheusPixlog <50592282+biscoitoman@users.noreply.github.com>
2023-01-11 20:07:53 -0300

    Front: Created a simple HTML page to test the API
    * Added a simple HTML with JavaScript page
    * Start it using express on the host
    * Compile and Run to test that the changes were valid

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const bodyParser = require('body-parser');
+const path = require('path');
 const express = require('express');
 const  {v1Routes}  = require('./api/v1');
 
@@ -9,6 +10,7 @@ app.use(bodyParser.json());
 const port = 5500;
 
 app.use('/v1', v1Routes);
+app.get('/', (req,res) => {res.sendFile(path.join(__dirname, '../html/index.html'))});
 
 app.listen(port, () => {
     console.log(`[App]: server listening at http://localhost:${port}`);
