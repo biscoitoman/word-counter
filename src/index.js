25bdf9cd844a36f520d85f37ffd43bf08f780d71
MatheusPixlog <50592282+biscoitoman@users.noreply.github.com>
2023-01-11 18:00:31 -0300

    Infra: Created Server express
    ChangeLog:
    
    * Created express server
    * Make changes on the template files to work with express
    * Compile and Run to test that the changes were valid

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const { v1Router } = require('./api/v1');
+const  {v1Routes}  = require('./api/v1');
 
 
 const app = express();
+const port = 5500;
 
-app.use('/v1/', v1Router);
+app.use('/v1/', v1Routes);
+
+app.listen(port, () => {
+    console.log(`[App]: server listening at http://localhost:${port}`);
+  });
 
 module.exports = {app};
